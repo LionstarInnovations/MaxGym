4d54c0a3abdf9bad4044c0257e019d964b0c3985
Marco <marcusmc787@gmail.com>
2015-12-20 10:22:30 +0000

    Added initial file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <tile>Max Gym</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Marcus McFarlane" />
+    <meta name="description" content="Max Gym fitness" />
+    <link rel="stylesheet" href="main.css" type="text/css" />
+</head>
+<body>
+    <!-- PHP -->
+    <!-- Java -->
+    <!-- JavaScript -->
+    <script src="scripts.js"></script>
+</body>
+</html>
